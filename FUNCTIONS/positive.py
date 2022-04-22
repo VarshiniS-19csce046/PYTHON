d9dc13e3d59d2a99cc11c359148ae6d069712e0d
VarshiniS-19csce046 <97889600+VarshiniS-19csce046@users.noreply.github.com>
2022-04-22 16:47:56 +0530

    Create positive.py

---
+++ FUNCTIONS/positive.py
@@ -0,0 +1,12 @@
+list=[int(no) for no in input("Enter numbers:").split()]
+def numbers(num):
+  pos= 0
+  neg=0
+  for j in num:
+      if(j>0):
+        pos=pos+1
+      else:
+        neg=neg+1
+  print("Number of positive numbers:",pos)
+  print("Number of negative number:",neg)
+numbers(list)
